73bd7332fcf4d9493350eccba1efefd5817bc2ac
jianmyphy123 <jianmyphy123@tutanota.com>
2017-10-26 03:34:56 +1100

    Post Model to json

--- src/blog/settings.py
+++ src/blog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'markdown_deux',
     'pagedown',
+    'rest_framework',
 
     # local apps
     'comments',
@@ -142,16 +143,3 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
-
-
-
-
-
-
-
-
-
-
-
-
-
