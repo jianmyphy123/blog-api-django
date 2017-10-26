fb607b65a3eb332da32ab183b1652073a0192e95
jianmyphy123 <jianmyphy123@tutanota.com>
2017-10-26 17:36:45 +1100

    RetrieveAPIView

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -2,7 +2,17 @@ from rest_framework.serializers import ModelSerializer
 
 from posts.models import Post
 
-class PostSerializer(ModelSerializer):
+class PostListSerializer(ModelSerializer):
+    class Meta:
+        model = Post
+        fields = [
+            'title',
+            'slug',
+            'content',
+            'publish'
+        ]
+
+class PostDetailSerializer(ModelSerializer):
     class Meta:
         model = Post
         fields = [
@@ -12,3 +22,19 @@ class PostSerializer(ModelSerializer):
             'content',
             'publish'
         ]
+"""
+
+data = {
+    "title": "Yeahh buddy",
+    "content": "New Content",
+    "publish": "2016-2-12",
+    "sulg": "yeah-buddy"
+}
+
+new_item = PostSerializer(data=data)
+if new_item.is_valid():
+    new_item.save()
+else
+    print(new_item.errors)
+
+"""
--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -1,8 +1,13 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from posts.models import Post
-from posts.api.serializers import PostSerializer
+from posts.api.serializers import PostDetailSerializer, PostListSerializer
 
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostListSerializer
+
+class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug'
