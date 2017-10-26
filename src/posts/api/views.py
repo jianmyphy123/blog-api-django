658a7ed0faddac45853cecd4381d357134afc2d9
jianmyphy123 <jianmyphy123@tutanota.com>
2017-10-26 17:55:24 +1100

    Update and Delete APIView

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -1,4 +1,9 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import (
+    DestroyAPIView,
+    ListAPIView,
+    UpdateAPIView,
+    RetrieveAPIView
+)
 
 from posts.models import Post
 from posts.api.serializers import PostDetailSerializer, PostListSerializer
@@ -6,6 +11,16 @@ from posts.api.serializers import PostDetailSerializer, PostListSerializer
 class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
+
+class PostUpdateAPIView(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug'
+
+class PostDeleteAPIView(DestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostDetailSerializer
+    lookup_field = 'slug'
 
 class PostDetailAPIView(RetrieveAPIView):
     queryset = Post.objects.all()
